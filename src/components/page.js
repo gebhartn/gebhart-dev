86104eb507b3c4ceaccc5e03b40ceab73dfc905e
gebhartn <nicholas.gebhart@gmail.com>
2020-11-06 18:49:05 -0800

    feat: add plugin scaffolding
    feat: add offline access
    
    chore: update eslintrc
    
    chore: update dependencies
    
    refactor: update gatsby config
    
    feat: add createpages api
    
    feat: add initial pages
    
    feat: add initial templates
    
    style: update directory

---
+++ src/components/page.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { graphql, Link } from 'gatsby'
+import { MDXProvider } from '@mdx-js/react'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+
+const shortcodes = { Link }
+export default function PageTemplate({ data: { mdx } }) {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div>
+      <h1>{mdx.frontmatter.title}</h1>
+      <MDXProvider components={shortcodes}>
+        <MDXRenderer>{mdx.body}</MDXRenderer>
+      </MDXProvider>
+    </div>
+  )
+}
+export const pageQuery = graphql`
+  query PageQuery($id: String) {
+    mdx(id: { eq: $id }) {
+      id
+      body
+      frontmatter {
+        title
+      }
+    }
+  }
+`
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default function Home() {
-  // eslint-disable-next-line react/jsx-filename-extension
-  return <div>Hello world!</div>
-}
