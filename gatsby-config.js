bdb72ee751159f86b36098940fb860796ba77b38
gebhartn <nicholas.gebhart@gmail.com>
2020-11-06 18:49:05 -0800

    feat: add plugin scaffolding
    feat: add offline access
    
    chore: update eslintrc
    
    chore: update dependencies
    
    refactor: update gatsby config
    
    feat: add createpages api
    
    feat: add initial pages
    
    feat: add initial templates
    
    style: update directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,34 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/content/pages/`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/content/posts/`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Nicholas Gebhart is a Software Engineer',
+        short_name: 'N. Gebhart',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        icons: [],
+      },
+    },
+  ],
 }
---
+++ gatsby-node.js
@@ -0,0 +1,61 @@
+const { createFilePath } = require('gatsby-source-filesystem')
+const path = require('path')
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'Mdx') {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: 'slug',
+      node,
+      value,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            frontmatter {
+              template
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    reporter.panicOnBuild('ERROR: Loading "createPages" query')
+  }
+
+  const all = result.data.allMdx.edges
+  const posts = all.filter(post => post.node.frontmatter.template === 'post')
+  const pages = all.filter(post => post.node.frontmatter.template === 'page')
+
+  posts.forEach(({ node }) => {
+    createPage({
+      path: `/blog${node.fields.slug}`,
+      component: path.resolve(`./src/components/post.js`),
+      context: { id: node.id },
+    })
+  })
+
+  pages.forEach(({ node }) => {
+    const checkPath = node.fields.slug === '/home/' ? '/' : node.fields.slug
+
+    createPage({
+      path: checkPath,
+      component: path.resolve(`./src/components/page.js`),
+      context: { id: node.id },
+    })
+  })
+}
